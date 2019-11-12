bce3856c1a6606f3980df0be4b4177b407afffe7
Panna Kristóf <kristofpanna@gmail.com>
2019-11-12 11:43:01 +0100

    Add backend project

---
+++ src/main/java/com/codecool/projectq/projectqbackend/ProjectqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.projectq.projectqbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectqBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectqBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/projectq/projectqbackend/ProjectqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.projectq.projectqbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectqBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
