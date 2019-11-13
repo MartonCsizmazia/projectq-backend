6528fa4eed530edaccca4b0228daee9d6cb5ed0d
Panna Kristóf <kristofpanna@gmail.com>
2019-11-13 17:52:32 +0100

    Fix CORS problem: add @Crossorigin; get map type instead of timestamp

--- src/main/java/com/codecool/projectq/projectqbackend/controller/QController.java
+++ src/main/java/com/codecool/projectq/projectqbackend/controller/QController.java
@@ -1,19 +1,19 @@
 package com.codecool.projectq.projectqbackend.controller;
 
-import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Timestamp;
+import java.util.HashMap;
 
-
+@CrossOrigin
 @RestController
 public class QController {
 
     @PostMapping("/requestnumber")
-    public int requestNumber(@RequestBody Timestamp time){
-        System.out.println("got it");
+    public int requestNumber(@RequestBody HashMap<String,String> map){
+        System.out.println("got it,time:  " + map.get("time") );
         return 123;
     }
 
