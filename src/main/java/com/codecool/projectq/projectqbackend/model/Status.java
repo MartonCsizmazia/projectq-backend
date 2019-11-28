8bb7548f67014eabf057eafb347962e8a8c2e245
Panna Kristóf <kristofpanna@gmail.com>
2019-11-28 11:16:15 +0100

    Add status to tickets

---
+++ src/main/java/com/codecool/projectq/projectqbackend/model/Status.java
@@ -0,0 +1,7 @@
+package com.codecool.projectq.projectqbackend.model;
+
+public enum Status {
+    WAITING,
+    HANDLING_TASK,
+    DONE
+}
--- src/main/java/com/codecool/projectq/projectqbackend/model/Ticket.java
+++ src/main/java/com/codecool/projectq/projectqbackend/model/Ticket.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 
 @Data
@@ -24,7 +21,13 @@ public class Ticket {
     private long timeOfRegistration;
     private long beforeMe;
     private long estimatedTimeOfAppointment;
+
+    @Enumerated(EnumType.STRING)
     private CaseType caseType;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Status status = Status.WAITING;
 
     @ManyToOne
     private Station station;
