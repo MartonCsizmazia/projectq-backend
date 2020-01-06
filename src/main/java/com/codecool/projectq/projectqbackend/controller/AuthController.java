61eb8a9c85109e0291ab5ef2a9a69336f3cf18f0
Enci21 <eni.hegedus.95@gmail.com>
2020-01-06 14:47:55 +0100

    fix cors error

--- src/main/java/com/codecool/projectq/projectqbackend/controller/AuthController.java
+++ src/main/java/com/codecool/projectq/projectqbackend/controller/AuthController.java
@@ -11,10 +11,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -23,6 +20,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
