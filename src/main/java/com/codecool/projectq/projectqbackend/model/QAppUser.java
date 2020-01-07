22ab6e00aeea2b82a3eceb1d4984ab0903584411
Marton Csizmazia <csmarton93@gmail.com>
2020-01-07 13:28:32 +0100

    logic of distance between office and user implemented

--- src/main/java/com/codecool/projectq/projectqbackend/model/QAppUser.java
+++ src/main/java/com/codecool/projectq/projectqbackend/model/QAppUser.java
@@ -1,5 +1,7 @@
 package com.codecool.projectq.projectqbackend.model;
 
+import com.codecool.projectq.projectqbackend.service.OfficeGetter;
+import com.codecool.projectq.projectqbackend.service.OfficeService;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Component
@@ -24,6 +27,8 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class QAppUser {
+
+    private OfficeGetter officeGetter;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
@@ -41,4 +46,45 @@ public class QAppUser {
 
     @OneToOne(mappedBy = "qAppUser")
     private CurrentPosition currentPosition;
+
+    private double distance(double lat1, double lon1, double lat2, double lon2) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0;
+        }
+        else {
+            double theta = lon1 - lon2;
+            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            dist = Math.acos(dist);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515;
+
+            dist = dist * 1.609344;
+
+            return (dist);
+        }
+    }
+
+    public HashMap <String, Integer> getDistanccesToOffices(){
+        List<Office> offices = officeGetter.getAllOffices();
+        HashMap <String, Integer> distanccesToOffices = new HashMap<>();
+        for (Office office : offices) {
+            //first point: current
+            //second point: office
+
+            //Hash_Map.put(key, value)
+            double officeLatitude = office.getLocation().getLatitude();
+            double officeLongitude = office.getLocation().getLongitude();
+
+            double userLatitude = currentPosition.getLatitude();
+            double userLongitude = currentPosition.getLongitude();
+
+            double distanceApproximately = Math.sqrt(Math.pow((office.getLocation().getLatitude()-currentPosition.getLatitude()),2)
+                                        +Math.pow((office.getLocation().getLongitude()-currentPosition.getLongitude()),2));
+
+            double distance = distance(userLatitude,userLongitude,officeLatitude,officeLongitude);
+
+            distanccesToOffices.put(office.getName(), (int) distance);
+        }
+        return distanccesToOffices;
+    }
 }
---
+++ src/main/java/com/codecool/projectq/projectqbackend/service/OfficeGetter.java
@@ -0,0 +1,10 @@
+package com.codecool.projectq.projectqbackend.service;
+
+import com.codecool.projectq.projectqbackend.model.Office;
+
+import java.util.List;
+
+public interface OfficeGetter {
+
+    List<Office> getAllOffices();
+}
